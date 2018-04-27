23c412d56b130c8227d985771de113cfc54521b5
姜建世 <jiangjianshi@192.168.0.107>
2018-04-27 22:53:24 +0800

    second commit

---
+++ src/main/java/com/hfq/house/manager/config/MyWebMvcConfig.java
@@ -0,0 +1,34 @@
+package com.hfq.house.manager.config;
+
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * 
+ * @author jjs
+ *
+ */
+@Configuration
+public class MyWebMvcConfig extends WebMvcConfigurerAdapter {
+
+	@Bean
+	public WebMvcConfigurerAdapter forwardToIndex() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addViewControllers(ViewControllerRegistry registry) {
+				registry.addViewController("/").setViewName("forward:/index");
+			}
+		};
+	}
+
+	@Override
+	public void configurePathMatch(PathMatchConfigurer configurer) {
+		configurer.setUseSuffixPatternMatch(false).setUseTrailingSlashMatch(false);
+	}
+
+}
